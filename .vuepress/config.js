2f963d1dffb1eec6279cfd08ebf4d0db16e6b414
jiesou <jiechushou@gmail.com>
2022-05-02 22:13:09 +0800

    优化SEO

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,20 +2,31 @@ module.exports = {
   locales: {
     '/': {
       lang: 'zh-CN',
-      title: 'LOLI IS TOP 萝莉赛高！',
+      title: 'LOLI IS TOP 萝莉赛高！'
     }
   },
+  markdown:{
+    externalLinks: { target: '_blank', rel: 'noopener noreferrer nofollow' }
+  },
   head: [
+    ['meta', { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' }]
     ['meta', { name: 'robots', content: 'index' }]
   ],
   themeConfig: {
     nav: [
-      {text: '萝莉色图', link: '/setu.html'},
-      {text: '赞助', link: '/donate.html'},
+      { text: '萝莉色图', link: '/setu.html'},
+      { text: '赞助', link: '/donate.html'},
     ],
      lastUpdated: '上次更新'
   },
   plugins: {
+    'autometa': {
+      site: {
+        name: 'LOLI IS TOP | 萝莉赛高 - 聚合导航',
+      },
+      canonical_base: 'https://loliis.top',
+    },
+    'vuepress-plugin-baidu-autopush': {},
     'sitemap': {
       hostname: 'https://loliis.top',
       exclude: [
@@ -24,12 +35,6 @@ module.exports = {
       urls: [
         { url: '/index.html', changefreq: 'montly'}
       ]
-    },
-    'autometa': {
-      site: {
-        name: 'LOLI IS TOP | 萝莉赛高 - 聚合导航',
-      },
-      canonical_base: 'https://loliis.top',
     }
   }
 }
