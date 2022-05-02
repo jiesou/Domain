5c3891eb78ac12f8f3b823214baf69f3d808d27c
jiesou <jiechushou@gmail.com>
2022-05-02 10:20:34 +0800

    添加优化

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,23 +1,16 @@
 module.exports = {
+  title: 'LOLI IS TOP 萝莉赛高！',
+  description: '萝莉二次元动漫图片API合集导航',
+  lang: 'zh-CN',
   head: [
     ['meta', { name: 'robots', content: 'index, nofollow' }]
   ],
   themeConfig: {
     sidebar: 'auto',
-    locales: {
-      '/': {
-        lang: 'zh-CN',
-        title: 'LOLI IS TOP 萝莉赛高！',
-        description: '萝莉二次元动漫图片API合集导航',
-        serviceWorker: {
-          updatePopup: {
-            message: "发现新内容可用.",
-            buttonText: "刷新"
-          }
-        },
-        lastUpdated: '上次更新',
-        
-      }
-    }
+    nav: [
+      {text: '萝莉色图', link: '/setu.html'},
+      {text: '赞助', link: '/donate.html'},
+    ],
+     lastUpdated: '上次更新'
   }
 }
