4c8012291e76c7c23b9e3f3dda0b3f72e204d8d6
jiesou <jiechushou@gmail.com>
2022-05-03 22:18:34 +0800

    修复统计和sitemap

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,9 +10,7 @@ module.exports = {
   },
   head: [
     ['meta', { name: 'robots', content: 'index' }],
-    ['script', {}, 'var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?69df9203fa509c9541b75ea58bf2de0d";var s = document.getElementsByTagName("script")[0];  s.parentNode.insertBefore(hm, s);})();'],
-    ['script', {}, '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "bqn7j88cgx");'],
-    ['script', {}, 'let sc=document.createElement("script");sc.async=true;sc.src="https://www.googletagmanager.com/gtag/js?id=G-W28PHGH59Y";document.body.appendChild(sc);window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "G-W28PHGH59Y");']
+    ['script', {}, 'var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?69df9203fa509c9541b75ea58bf2de0d";var s = document.getElementsByTagName("script")[0];  s.parentNode.insertBefore(hm, s);})();']
   ],
   themeConfig: {
     sidebar: 'auto',
@@ -35,7 +33,7 @@ module.exports = {
         '/404.html'
       ],
       urls: [
-        { url: '/index.html', changefreq: 'montly'}
+        { url: '/index.html'}
       ]
     }
   }
